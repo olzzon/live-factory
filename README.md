f75de4d8dafd877f555ca5afbb1a848db6082c8c
olzzon <github@olzzon.dk>
2022-03-13 18:45:08 +0100

    feat: Mpeg-ts input

--- README.md
+++ README.md
@@ -5,8 +5,27 @@ Reciever - transmitter - transcoder for video and audio
 -f lavfi -i smptehdbars=1920x1080
 
 
+## Mac M1 FFmpeg compilation:
+### Install SRT support:
+```
 
-### Build FFMPEG for Mac M1
+git clone --depth 1 https://github.com/Haivision/srt.git ~/ffmpeg_sources/srt
+cd ~/ffmpeg_sources/srt
+
+brew unlink openssl && brew link openssl --force
+echo 'export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"' >> ~/.zshrc
+# For macOS also export OpenSSL paths with the following commands:
+# export OPENSSL_ROOT_DIR=$(brew --prefix openssl)
+# export OPENSSL_LIB_DIR=$(brew --prefix openssl)"/lib"
+# export OPENSSL_INCLUDE_DIR=$(brew --prefix openssl)"/include"
+
+./configure
+make
+sudo make install
+
+```
+
+### Build FFMPEG
 Follow https://trac.ffmpeg.org/wiki/CompilationGuide/macOS
 
 Add dependencies:
@@ -17,19 +36,29 @@ brew install automake fdk-aac git lame libass libtool libvorbis libvpx opus sdl 
 install: http://new.tk/NDIRedistV5Apple
 ```
 
-Remember to run:
+#### Clone NDI-FFMPEG:
+```
+https://github.com/olzzon/ffmpeg-ndi
+```
+
+```
+./configure  --prefix=/usr/local --enable-libsrt --enable-gpl --enable-nonfree --enable-libass --enable-libfdk-aac --enable-libfreetype --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-libopus --samples=fate-suite --enable-videotoolbox --enable-libndi_newtek 
+```
+
 ```
 make distclean 
+make
+sudo make install
 ```
 
+Rename ffmpeg to ffmpegruntime and copy it to "dist/server" folder
 
-```
-./configure  --prefix=/usr/local --enable-gpl --enable-nonfree --enable-libass --enable-libfdk-aac --enable-libfreetype --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 --enable-libopus --samples=fate-suite --enable-videotoolbox --enable-libndi_newtek --enable-hwaccel=h264  
-```
 
+
+## FFmpeg Examples
 Decode HW:
 ```
 ./ffmpeg -stream_loop -1 -hwaccel videotoolbox -hwaccel_output_format videotoolbox -re -vsync 0 -i /Users/olzzon/coding/live-factory/media/RedBull.ts -f libndi_newtek -pix_fmt uyvy422 OUTPUT
 ```
-### Patch FFMPEG:
+### Patch NDI - FFMPEG:
 https://gist.github.com/pabloko/8affc8157a83e201503c326a28d17bf2
--- src/client/Components/InputTypes/Mpegts.tsx
+++ src/client/Components/InputTypes/Mpegts.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { storeClearGlobalParams, storeClearInputParams, storeSetInputParams } from '../../../interface/redux/containerActions'
+import {
+	storeSetGlobalParams,
+	storeSetInputParams,
+} from '../../../interface/redux/containerActions'
 import { RootState } from '../../main'
 
 interface IMpegtsProps {
@@ -11,22 +14,54 @@ const MpegtsInputOptions: React.FC<IMpegtsProps> = (props) => {
 	const dispatch = useDispatch()
 	const id = props.factoryId
 
-	useEffect(() => {
-		dispatch(storeClearGlobalParams(id))
-		dispatch(storeClearInputParams(id))
+	const ip = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].input.params[1])
+	const port = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].input.params[3])
+	const fifoSize = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].input.params[5])
 
-		dispatch(storeSetInputParams(id, 0, ' -i udp://'))
-		dispatch(storeSetInputParams(id, 1, 'localhost:1234'))
+	useEffect(() => {
+		dispatch(storeSetGlobalParams(id, 0, ' -re '))
+		dispatch(storeSetInputParams(id, 0, '-hwaccel videotoolbox -vsync 0 -i udp://'))
+		if (!ip) {
+			dispatch(storeSetInputParams(id, 1, 'localhost'))
+		}
+		dispatch(storeSetInputParams(id, 2, ':'))
+		if (!port) {
+			dispatch(storeSetInputParams(id, 3, '1234'))
+		}
+		dispatch(storeSetInputParams(id, 4, '?fifo_size='))
+		if (!fifoSize) {
+			dispatch(storeSetInputParams(id, 5, '49152'))
+		}
 	}, [])
 
-	const url = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].input.params[1])
-
-
 	return (
-		<div className='options'>
-			<label className="">
-				URL :
-				<input className="" type="text" value={url} onChange={(event) => dispatch(storeSetInputParams(id, 1, event.target.value))} />
+		<div className="options">
+			<label className="pipeline-label">
+				IP :
+				<input
+					className="input-text"
+					type="text"
+					value={ip}
+					onChange={(event) => dispatch(storeSetInputParams(id, 1, event.target.value))}
+				/>
+			</label>
+			<label className="pipeline-label">
+				PORT :
+				<input
+					className="input-text"
+					type="text"
+					value={port}
+					onChange={(event) => dispatch(storeSetInputParams(id, 3, event.target.value))}
+				/>
+			</label>
+			<label className="pipeline-label">
+				Buffer Size (fifo) :
+				<input
+					className="input-text"
+					type="text"
+					value={fifoSize}
+					onChange={(event) => dispatch(storeSetInputParams(id, 5, event.target.value))}
+				/>
 			</label>
 		</div>
 	)
