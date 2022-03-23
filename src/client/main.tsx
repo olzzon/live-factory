5b77e1a12d92f506d1fb544916a916a09978bac2
olzzon <github@olzzon.dk>
2022-03-23 17:30:14 +0100

    feat: olzzon logo

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,16 +4,20 @@ import React from 'react'
 import { store } from '../interface/redux/store'
 import { Provider } from 'react-redux'
 import FactoryHandler from './Components/FactoryHandler'
+//@ts-ignore
+import logo from './assets/logo_black.png'
+
 export type RootState = ReturnType<typeof store.getState>
 
 ReactDOM.render(
-    <Provider store={store}>
+	<Provider store={store}>
 		<div className="header">
 			<hr />
-			LIVE FACTORY
+			<img className={'logo'} src={logo} width="110" height="45" />
+			NDI FACTORY
 			<hr />
 		</div>
-		<FactoryHandler/>
+		<FactoryHandler />
 	</Provider>,
 	document.getElementById('app-root')
 )
