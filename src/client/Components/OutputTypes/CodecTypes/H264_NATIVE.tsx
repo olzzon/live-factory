5f86e518200d4ffc15d1fde79b7331118af410fd
olzzon <github@olzzon.dk>
2022-05-18 13:24:30 +0300

    Feat: Add de-interlace support for x264

--- src/client/Components/OutputTypes/CodecTypes/H264_NATIVE.tsx
+++ src/client/Components/OutputTypes/CodecTypes/H264_NATIVE.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
 	storeSetFilterParams,
@@ -16,6 +16,9 @@ const H264NativeCodecOptions: React.FC<ICodecProps> = (props) => {
 
 	const vBandwidth = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].filter.paramArgs[0])
 	const aBandwidth = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].filter.paramArgs[1])
+	const quality = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].filter.paramArgs[2])
+	const deInterlace = useSelector<RootState, string>((state) => state.ffmpeg[0].factory[id].filter.paramArgs[3])
+	const [ deinterlaceState, setDeinterlaceState ] = useState<boolean>((deInterlace === '-vf yadif')? true : false)
 
 	useEffect(() => {
 		//` -re -i srt://0.0.0.0:9998?pkt_size=1316&mode=listener -vcodec copy -acodec copy -strict -2 -y`))
@@ -24,7 +27,7 @@ const H264NativeCodecOptions: React.FC<ICodecProps> = (props) => {
 		// CUDA Linux:
 		//  ` -c:v h264_nvenc -preset llhq -zerolatency 1 -b:v 6000k -pix_fmt yuv420p `))
 		// -c:v libx264 -preset veryfast -b:v 22000k -pix_fmt yuv420p -acodec libopus -b:a 256k
-		dispatch(storeSetFilterParamString(id, ` -c:v libx264 -preset veryfast -b:v {arg0}k -pix_fmt yuv420p -acodec libopus -b:a {arg1}k`))
+		dispatch(storeSetFilterParamString(id, ` {arg3} -c:v libx264 -preset veryfast -b:v {arg0}k -pix_fmt yuv420p -acodec libopus -b:a {arg1}k`))
 
 		if (!vBandwidth) {
 			dispatch(storeSetFilterParams(id, 0, `22000`))
@@ -32,10 +35,37 @@ const H264NativeCodecOptions: React.FC<ICodecProps> = (props) => {
 		if (!aBandwidth) {
 			dispatch(storeSetFilterParams(id, 1, `256`))	
 		}
+		if (!quality) {
+			dispatch(storeSetFilterParams(id, 2, `90`))	
+		}
+		if (!deinterlaceState) {
+			dispatch(storeSetFilterParams(id, 3, ` `))
+		} else {
+			dispatch(storeSetFilterParams(id, 3, `-vf yadif`))
+		}
 	}, [])
+
+	const handleSetDeInterlace = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.checked) {
+			dispatch(storeSetFilterParams(id, 3, '-vf yadif'))
+			setDeinterlaceState(true)			
+		} else {
+			dispatch(storeSetFilterParams(id, 3, ' '))
+			setDeinterlaceState(false)			
+		}
+	}
 
 	return (
 		<div className="options">
+						<label className="pipeline-label">
+				De-interlace :
+				<input
+					className="input-checkbox"
+					type="checkbox"
+					checked={deinterlaceState}
+					onChange={(event) => handleSetDeInterlace(event)}
+				/>
+			</label>
 			<label className="pipeline-label">
 				Video Bandwidth (in kbit/s) :
 				<input
@@ -52,6 +82,15 @@ const H264NativeCodecOptions: React.FC<ICodecProps> = (props) => {
 					type="number"
 					value={aBandwidth ?? '256'}
 					onChange={(event) => dispatch(storeSetFilterParams(id, 1, event.target.value))}
+				/>
+			</label>
+			<label className="pipeline-label">
+				Quality (1-100) :
+				<input
+					className="input-number"
+					type="number"
+					value={quality ?? '90'}
+					onChange={(event) => dispatch(storeSetFilterParams(id, 2, event.target.value))}
 				/>
 			</label>
 		</div>
